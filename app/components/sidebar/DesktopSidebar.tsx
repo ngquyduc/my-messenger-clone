a1e9ca8f9f9ecb4c55a353311f3f094169ab74e6
Duc Nguyen <quyduc711@gmail.com>
2023-07-13 14:38:23 +0800

    add avatar display and people pages

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import useRoutes from '@/app/hooks/useRoutes'
+import { User } from '@prisma/client'
 import { useState } from 'react'
+import Avatar from '../Avatar'
 import DesktopItem from './DesktopItem'
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
+  console.log({ currentUser })
+
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
       <nav className="mt-4 flex-4 flex-col justify-between">
@@ -22,6 +30,14 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   )
